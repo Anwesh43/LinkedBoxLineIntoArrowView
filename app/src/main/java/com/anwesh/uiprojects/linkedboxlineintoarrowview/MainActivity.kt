e6ece050b31c962f954c06eec9a5c0e7f22d98d8
Anwesh43 <anweshthecool0@gmail.com>
2019-06-06 10:30:25 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedboxlineintoarrowview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedboxlineintoarrowview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedboxlineintoarrowview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.boxlineintoarrowview.BoxLineIntoArrowView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        BoxLineIntoArrowView.create(this)
     }
 }
