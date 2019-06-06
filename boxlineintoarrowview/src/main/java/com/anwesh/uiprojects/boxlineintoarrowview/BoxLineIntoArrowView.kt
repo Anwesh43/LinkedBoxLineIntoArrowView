c575379c3c0c067345157edc9b1cbd6cf5aa07c4
Anwesh43 <anweshthecool0@gmail.com>
2019-06-06 09:53:53 +0530

    adding Libraries and constants

--- boxlineintoarrowview/src/main/java/com/anwesh/uiprojects/boxlineintoarrowview/BoxLineIntoArrowView.kt
+++ boxlineintoarrowview/src/main/java/com/anwesh/uiprojects/boxlineintoarrowview/BoxLineIntoArrowView.kt
@@ -3,3 +3,21 @@ package com.anwesh.uiprojects.boxlineintoarrowview
 /**
  * Created by anweshmishra on 06/06/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+import android.content.Context
+
+val nodes : Int = 5
+val lines : Int = 2
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#4527A0")
+val backColor : Int = Color.parseColor("#BDBDBD")
