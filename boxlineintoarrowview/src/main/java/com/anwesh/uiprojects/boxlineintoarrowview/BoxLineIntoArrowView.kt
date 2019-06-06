4fbc32b53ba651e8f80e5f2602ef985eb24d6580
Anwesh43 <anweshthecool0@gmail.com>
2019-06-06 10:18:45 +0530

    adding LIANode class

--- boxlineintoarrowview/src/main/java/com/anwesh/uiprojects/boxlineintoarrowview/BoxLineIntoArrowView.kt
+++ boxlineintoarrowview/src/main/java/com/anwesh/uiprojects/boxlineintoarrowview/BoxLineIntoArrowView.kt
@@ -129,4 +129,48 @@ class BoxLineIntoArrowView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LIANode(var i : Int, val state : State = State()) {
+
+        private var next : LIANode? = null
+        private var prev : LIANode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = LIANode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLIANode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LIANode {
+            var curr : LIANode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this 
+        }
+    }
 }
